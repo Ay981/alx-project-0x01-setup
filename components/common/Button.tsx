64d5193d24667b2df609c7a0600ac7b9becd4baf
ay981 <aymenabdulkerim411@gmail.com>
2025-07-19 22:53:22 +0300

    commit

---
+++ components/common/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
+  <button {...props} className={`px-4 py-2 bg-blue-500 text-white rounded ${props.className || ''}`}>
+    {children}
+  </button>
+);
+
+export default Button;
---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="w-full p-4 bg-gray-800 text-white text-center mt-auto">
+    <p className="text-sm">&copy; 2025 alx-project-0x01</p>
+  </footer>
+);
+
+export default Footer;
