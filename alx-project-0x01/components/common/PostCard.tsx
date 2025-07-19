f71e4cbe6f24801891ee0677a02f096a554a99a8
ay981 <aymenabdulkerim411@gmail.com>
2025-07-19 23:10:37 +0300

    fix

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
 
-type PostCardProps = {
-  title: string;
-  content: string;
-};
-
-const PostCard: React.FC<PostCardProps> = ({ title, content }) => (
-  <div className="border rounded p-4 shadow-md bg-white">
-    <h2 className="text-xl font-semibold mb-2">{title}</h2>
-    <p>{content}</p>
-  </div>
-);
-
-export default PostCard;
