154bbe9e170c9b4ddb55f404a272a9ddf6d0927e
ay981 <aymenabdulkerim411@gmail.com>
2025-07-19 22:59:53 +0300

    commit

---
+++ pages/users/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "@/components/layout/Header";
+
+const Users: React.FC = () => (
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-grow p-8 bg-gray-50">
+      <h1 className="text-3xl font-bold mb-4">Users Page</h1>
+      <p>List of users will appear here.</p>
+    </main>
+  </div>
+);
+
+export default Users;
